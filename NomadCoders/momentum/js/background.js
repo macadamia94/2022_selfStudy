b78149b81eda0350e182c549ea1ac2ec07f7a4b1
macadamia94 <thswlgus15@hanmail.net>
2022-06-26 20:28:49 +0900

    css 미완

--- NomadCoders/momentum/js/background.js
+++ NomadCoders/momentum/js/background.js
@@ -1,3 +1,8 @@
+/* 
+const UNSPLASH_API_KEY = "a1xZRBE9bmyz5AGmwH2E2xYhJBy2GZbi9m5DjJrkFSA"
+const UNSPLASH_URL = `https://api.unsplash.com/photos/random/?client_id=${UNSPLASH_API_KEY}&query=landscape&orientation=landscape`;
+*/
+
 const images = ["0.jpg", "1.jpg", "2.jpg", "3.jpeg", "4.jpeg", "5.jpeg"];
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 const bgImage = document.createElement("img");
